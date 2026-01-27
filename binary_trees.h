a4c343b14b36cb129d021171e44a9e035fc3a21a
Izzat <shixbalayevizzet@example.com>
2026-01-27 11:34:53 +0000

    task0

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,17 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
     int n;
@@ -5,4 +19,11 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
+
 typedef struct binary_tree_s binary_tree_t;
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+void binary_tree_print(const binary_tree_t *);
+
+#endif
