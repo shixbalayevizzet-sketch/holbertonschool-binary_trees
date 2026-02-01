e89f7f3ce0ebf462a404e45a3b2c00719bd3ce33
Izzat <shixbalayevizzet@example.com>
2026-02-01 10:32:20 +0000

    task10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,10 +9,10 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL || tree->parent == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL || tree->parent == NULL)
+	{
+		return (0);
+	}
 
-    return (1 + binary_tree_depth(tree->parent));
+	return (1 + binary_tree_depth(tree->parent));
 }
